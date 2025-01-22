e2adef7211cef5ffaa1cca488b7dcb9950b98432
Van5sh <vansh05dhir@gmail.com>
2025-01-22 15:29:50 +0530

    android folder and the name of the app

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Stack = createNativeStackNavigator();
+const Bottom = createBottomTabNavigator();
 
 export default function App() {
   return (
