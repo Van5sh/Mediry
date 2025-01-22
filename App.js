498371c208eb479c2a089e2c3ff89c643b23e58d
Van5sh <vansh05dhir@gmail.com>
2025-01-22 17:24:52 +0530

    icon added

--- App.js
+++ App.js
@@ -9,10 +9,9 @@ const Bottom = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+        
+    </>
   );
 }
 
